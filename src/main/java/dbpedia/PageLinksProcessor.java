2704ea8adf6a1c75bd021e75a4435b25bade818b
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2015-09-30 16:46:08 +0200

    pagelinks count

--- src/main/java/dbpedia/PageLinksProcessor.java
+++ src/main/java/dbpedia/PageLinksProcessor.java
@@ -10,6 +10,7 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.RDFS;
 import java.io.BufferedWriter;
@@ -19,6 +20,9 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.rdfhdt.hdt.hdt.HDT;
@@ -81,6 +85,90 @@ public class PageLinksProcessor {
             hdt = HDTManager.mapIndexedHDT(dataLoc, null);
             graph = new HDTGraph(hdt);
             model = ModelFactory.createModelForGraph(graph);
+            
+            HashMap<String, Instance> hm = new HashMap();
+
+            System.out.println("data loaded");
+            
+            String sparql = "SELECT distinct ?obj WHERE { " +
+                        "?subj  <http://dbpedia.org/ontology/wikiPageWikiLink> ?obj . "
+                    + "} ";
+            System.out.println("now query");
+            Query qry = QueryFactory.create(sparql);
+            QueryExecution qe = QueryExecutionFactory.create(qry, model);
+            ResultSet rs = qe.execSelect();
+            System.out.println("listing");
+
+            while(rs.hasNext()) {
+                QuerySolution sol = rs.nextSolution();
+                Resource subj = sol.get("subk").asResource();
+                Resource obj = sol.get("obj").asResource();
+                
+                if(!hm.containsKey(subj.getURI())) {
+                    Instance i = new Instance();
+                    i.setCount(1);
+                    i.setLink(subj.getURI());
+                    hm.put(subj.getURI(), i);
+                }
+                
+                if(!hm.containsKey(obj.getURI())) {
+                    Instance i = new Instance();
+                    i.setCount(1);
+                    i.setLink(obj.getURI());
+                    hm.put(obj.getURI(), i);
+                }
+//                System.out.println(obj);
+                
+//                StmtIterator iter = model.listStatements(null, model.getProperty("http://dbpedia.org/ontology/wikiPageWikiLink"), obj.asResource());
+//                int count = 0;
+//                while(iter.hasNext()) {
+//                    iter.nextStatement();
+//                    count++;
+//                    totalCounter++;
+//                }
+//
+//                StmtIterator labelIter = model.listStatements(obj.asResource(), RDFS.label, (RDFNode)null);
+//                if(labelIter.hasNext()) {
+////                    myobj.setLabel(labelIter.nextStatement().getObject().asLiteral().toString());
+//                    out.write(labelIter.nextStatement().getObject().asLiteral().getValue()+"\t"+obj.asResource().getURI()+"\t"+count+"\n");
+//                    out.flush();
+//                }
+            }
+            
+            Iterator it = hm.entrySet().iterator();
+            
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();                
+//                System.out.println(pair.getKey() + " = " + pair.getValue());
+                Instance i = (Instance)pair.getKey();
+                StmtIterator iter = model.listStatements(null, model.getProperty("http://dbpedia.org/ontology/wikiPageWikiLink"), model.getResource(i.getLink()));
+                while(iter.hasNext()) {
+                    iter.nextStatement();
+                    i.setCount(i.getCount()+1);
+                }
+                it.remove(); // avoids a ConcurrentModificationException
+                out.write(i.getLink()+"\t"+i.getCount()+"\n");
+            }
+            System.out.println(totalCounter);
+            out.close();
+
+            qe.close(); 
+        } catch (IOException ex) {
+            Logger.getLogger(PageLinksProcessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void processInLinksCountsBK(String lang, String dataLoc) {
+        try {
+            HDT hdt;
+            HDTGraph graph;
+            Model model;
+            int totalCounter = 0;
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(dataLoc+".inlink", true)));
+            
+            hdt = HDTManager.mapIndexedHDT(dataLoc, null);
+            graph = new HDTGraph(hdt);
+            model = ModelFactory.createModelForGraph(graph);
+
             System.out.println("data loaded");
 //            HDT hdtLabels = HDTManager.mapIndexedHDT("", null);
 //            HDTGraph graphLabels = new HDTGraph(hdtLabels);
@@ -236,6 +324,39 @@ public class PageLinksProcessor {
          */
         public void setLabel(String label) {
             this.label = label;
+        }
+    }
+    
+    public class Instance {
+        private int count;
+        private String link;
+
+        /**
+         * @return the count
+         */
+        public int getCount() {
+            return count;
+        }
+
+        /**
+         * @param count the count to set
+         */
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        /**
+         * @return the link
+         */
+        public String getLink() {
+            return link;
+        }
+
+        /**
+         * @param link the link to set
+         */
+        public void setLink(String link) {
+            this.link = link;
         }
     }
 
